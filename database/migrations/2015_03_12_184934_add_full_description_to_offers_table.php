ea1e638da599184b49a235d06f8c8008b207c4d2
Nick Mitchell <nick@kitset.io>
2017-09-27 18:25:22 +1300

    commit from Mitchy

--- database/migrations/2015_03_12_184934_add_full_description_to_offers_table.php
+++ database/migrations/2015_03_12_184934_add_full_description_to_offers_table.php
@@ -14,7 +14,7 @@ class AddFullDescriptionToOffersTable extends Migration
     public function up()
     {
         Schema::table('offers', function (Blueprint $table) {
-            $table->text('full_description')->after('description');
+            $table->text('full_description')->after('description')->nullable()->default('Full Description goes here');
         });
     }
 
